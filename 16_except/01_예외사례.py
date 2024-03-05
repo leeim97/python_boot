b9db685218712d862d1d39d308247029e2ea4917
Imhyung Lee <rhdtn987@gmail.com>
2024-03-05 10:13:05 +0900

    ck2

---
+++ 16_except/01_예외사례.py
@@ -0,0 +1,38 @@
+# ZeroDivisionError: division by zero
+
+# 10/0
+
+# TypeError
+# print('나이는' + 23 + '살')
+
+# NameError
+# print(b)
+
+# ValueError: incomplete format
+c = 100
+# print('%d%' % c)
+
+# SyntaxError
+# if x > 10
+#     print('hello')
+
+# IndexError
+a = [1,2,3,4]
+# print(a[5])
+
+# UnboundLocalError
+def add():
+    a = a+1
+
+# add()
+
+# ModuleNotFoundError
+# import mymodul
+
+# FileNotFoundError
+# with open('except.txt','r') as f:
+#     f.read()
+
+# OSError
+# with open('c:\file\except.txt','r') as f:
+#     f.read()
--- practice.py
+++ practice.py
@@ -1,26 +1,9 @@
-class Dog():
-    def __init__(self,name,breed,size,age,color):
-        self.name=name
-        self.breed=breed
-        self.size=size
-        self.age=age
-        self.color=color
-
-    def eat(self,food):
-        print(f'{self.name}이 {food}를 먹는다')
-
-    def sleep(self):
-        print(f'{self.name}이 잠잔다')
-
-    def sit(self):
-        print(f'{self.name}이 앉아있다')
-
-    def run(self):
-        print(f'{self.name}이 뛴다')
-
-dog1 = Dog('삐삐','Maltese','small','2','white')
-dog2 = Dog('벤ㅇ','chow chow','medium','3','brown')
-dog3 = Dog('뭉치','mastiff','large','5','black')
-
-dog1.eat('fddo')
-dog1.sleep()
+def test(x, y):
+    tmp = x
+    x = y
+    y = tmp
+    return y.append(x)
+x = ["y"]
+y = ["x"]
+test(x, y)
+print(x)
